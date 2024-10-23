0030d77d0208194a35541b2dc13606ebdc65bb4e
Alexander Ivanov <Mossd925@gmail.com>
2024-10-23 12:34:36 +0300

    some changes

--- lib/gms_services.dart
+++ lib/gms_services.dart
@@ -1,7 +1,33 @@
 library gms_services;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:platform_dependent_services/platform_dependencies.dart';
+
+class PlatformService implements PlatformDependencies {
+  @override
+  Analytics get analytics => GmsAnalytics();
+
+  @override
+  // TODO: implement core
+  Core get core => throw UnimplementedError();
+
+  @override
+  // TODO: implement messaging
+  Messaging get messaging => throw UnimplementedError();
+
+  @override
+  // TODO: implement storages
+  Storages get storages => throw UnimplementedError();
+}
+
+class GmsAnalytics implements Analytics {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+
+  @override
+  Future<void> init() {
+    // TODO: implement init
+    throw UnimplementedError();
+  }
 }
